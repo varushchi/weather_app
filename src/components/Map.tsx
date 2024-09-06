074d21fbba91f9d8503e7e3a1d0db1298075b1b4
varushchi <vadim.shchigolev@mail.ru>
2024-09-06 19:03:49 +0300

    added city name

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './Map.css'
 import { MapContainer, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
-import axios from 'axios';
 import MyMarker from './MyMarker';
 
 interface Props{
-  userLocation: {
+  center:{
     lat: number
-    lon: number
+    lng: number
   }
   handlePosition: (position: {lat: number, lng: number}) => void
 }
 
-function Map({userLocation, handlePosition}: Props){
+function Map({handlePosition, center}: Props){
 
-  const [position, setPosition] = useState<undefined | {lat: number, lng: number}>()
+  const [position, setPosition] = useState<{lat: number, lng: number}>(center)
 
   return (
     <div className='Map'>
-        <MapContainer center={[userLocation.lat, userLocation.lon]} zoom={7} scrollWheelZoom={true} style={{ height: "100%", minHeight: "100px" }}>
+        <MapContainer center={position} zoom={7} scrollWheelZoom={true} style={{ height: "100%", minHeight: "100px" }}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <MyMarker center = {{lat: userLocation.lat, lng: userLocation.lon}} handleDrag = {(position: {lat: number, lng: number}) => {
-            setPosition({lat: Number(position.lat.toFixed(3)), lng: Number(position.lng.toFixed(3))})
-            handlePosition(position)
-          }}/>
+            <MyMarker center = {position} handleDrag = {(position: {lat: number, lng: number}) => {
+              setPosition({lat: Number(position.lat.toFixed(3)), lng: Number(position.lng.toFixed(3))})
+              handlePosition(position)
+            }}/>
+          
         </MapContainer>
       <div className='position-div'>
-        {position && position.lat}   {position && position.lng}
+        {position.lat.toFixed(3)}   {position.lng.toFixed(3)}
       </div>
     </div>
   )
--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -15,62 +15,69 @@ export default function Weather() {
       temperature_2m: string
       time?: string
     }
+    hourly_units?: {
+      time: string
+      temperature_2m: string
+      precipitation_probability:string
+      rain: string
+      snowfall: string
+      cloud_cover: string
+      wind_speed_10m: string
+    }
+    hourly?:
+    {
+      time: [string]
+      temperature_2m: [number]
+      precipitation_probability: [number]
+      snowfall: [number]
+      cloud_cover: [number]
+      wind_speed_10m: [number]
+    }
   }
 
-  interface GetUserLocation{
-    as?: string
-    city?: string
-    country?: string
-    countryCode?: string
-    isp?: string
-    lat: number
-    lon: number
-    org?: string
-    query?: string
-    region?: string
-    regionName?: string
-    status?: string
-    timezone?: string
-    zip?:string
+  interface GeoData{
+    continent: string
+    countryName: string
+    city: string
   }
 
-  const [userLocation, setUserLocation] = useState<undefined | GetUserLocation>()
-  const [position, setPosition] = useState<undefined | {lat: number, lng: number}>()
+  const [position, setPosition] = useState<{lat: number, lng: number}>({lat: 55.751, lng: 37.617})
   const [weather, setWeather] = useState<undefined | WeatherData>()
+  const [geoData, setGeoData] = useState<undefined | GeoData>()
   const [showMap, setShowMap] = useState(false)
 
   useEffect(() => {
-    async function getLocation() {
-      const res = await axios.get("http://ip-api.com/json");
-      if (res.status === 200){
-        setUserLocation(res.data)
-      }
-      else
-        setUserLocation({lat: 50, lon: 0})
-    }
-    getLocation();
-  }, []);
-
-
-  useEffect(() => {
     async function getWeather(){
-      const lat = position ? position.lat : 50
-      const lon = position ? position.lng : 0
-      const res = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m`)
+      const lat = position.lat
+      const lng = position.lng
+      const res = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&current=temperature_2m&hourly=temperature_2m,precipitation_probability,rain,snowfall,cloud_cover,wind_speed_10m`)
       if (res.status === 200)
         setWeather(res.data)
       else if (res.status === 400)
         setWeather({current: {temperature_2m: 0}, current_units: {temperature_2m: '°C'}})
     }
     getWeather()
+
+    async function getGeoData(){
+      const lat = position ? position.lat : 50
+      const lng = position ? position.lng : 0
+      const res = await axios.get(`https://api-bdc.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`)
+      if (res.status === 200)
+        setGeoData(res.data)
+    }
+    getGeoData()
   },[position])
 
+  useEffect(() => {
+    
+  }, [position])
 
   return (
     <div>
-      {`${weather?.current.temperature_2m} ${weather?.current_units.temperature_2m}`}
+      {geoData && `Temperature in ${geoData.countryName}, ${geoData.city} is: `}
+      {geoData?.city && weather && `${weather.current.temperature_2m} ${weather.current_units.temperature_2m}`}
       <button onClick={() => setShowMap(!showMap)}>set location</button>
-      {showMap && userLocation && <Map userLocation={{lat: userLocation.lat , lon: userLocation.lon}} handlePosition = {(position: {lat: number, lng: number}) => setPosition(position)}/>}
+      {showMap && <Map center={position} handlePosition = {(position: {lat: number, lng: number}) => setPosition(position)}/>}
     </div>
   )
 }
