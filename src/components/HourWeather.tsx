44cf8aaee6de62a92e971617f76b5ae8770d2ab1
varushchi <vadim.shchigolev@mail.ru>
2024-09-10 00:30:51 +0300

    installed charts for later

--- src/components/HourWeather.tsx
+++ src/components/HourWeather.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './HourWeather.css'
+import { LineChart } from '@mui/x-charts'
 
 interface Props{
   key : string
@@ -29,6 +30,12 @@ export default function HourWeather(props: Props) {
           Rain: {props.rain}mm
         </li>
       </ul>
+      <LineChart
+        xAxis={[{data: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]}]}
+        series={[{data: [1,2,3,4,5,6,7,8,20,1,30]}]}
+        width={500}
+        height={300}
+      />
     </div>
   )
 }
